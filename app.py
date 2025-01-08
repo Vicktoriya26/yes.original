1692fa80f325f0e651a18b689b0c570b35876bf4
Vicktoriya26 <vkoval4004@gmail.com>
2025-01-08 20:11:55 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, flash
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+from db_scripts import DatabaseManager
+
+app = Flask(__name__)  # Створюємо веб–додаток Flask
+app.secret_key = os.getenv('SECRET_KEY')
+db = DatabaseManager("blog.db")
+
+IMG_PATH = os.path.dirname(__file__) + os.sep + "static" + os.sep + "img"
+
+
+@app.context_processor
+def get_categories():
+    categories = db.get_all_categories()
+    return dict(categories=categories)
+
+@app.route("/")  # Вказуємо url-адресу для виклику функції
+def index():
+    products = db.get_all_products()
+    return render_template("index.html", products=products)
+
+
+@app.route("/products/<int:products_id>")  # Вказуємо url-адресу для виклику функції
+def products_page(products_id):
+    products = db.get_products(products_id)
+    return render_template("products_page.html", products=products) 
+
+
+
+@app.route("/categories/<int:category_id>")  # Вказуємо url-адресу для виклику функції
+def category_page(category_id):
+    products = db.get_products(category_id)
+    return render_template("index.html", products=products) 
+
+@app.route("/products/new", methods=["GET", "POST"])
+def new_products():
+    if request.method == 'POST':
+        image = request.file['image']
+        image.save(IMG_PATH + image.filename)
+        db.add_products(request.form['title'], request.form['content'],
+                        image.filename, 1, request.form['category'])
+        flash("Статтю додано")
+    else:
+        flash("Виберіть статтю заповніть всі поля.")
+    return render_template("new_products.html") 
+
+
+@app.route("/search")  # Вказуємо url-адресу для виклику функції
+def search():
+    products = db.get_all_products()
+
+    if request.method == 'GET':
+        query = request.args.get("query")
+        products = db.search_products(query)
+
+    return render_template("index.html", products=products)
+
+
+
+
+
+if __name__ == "__main__":
+    app.config['TEMPLATES_AUTO_RELOAD'] = True  # автоматичне оновлення шаблонів
+    app.run(debug=True)  # Запускаємо веб-сервер з цього файлу в режимі налагодження
---
+++ db_scripts.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+class DatabaseManager:
+
+    def __init__(self, dbname):
+        self.conn = None
+        self.cursor = None
+        self.dbname = dbname
+
+
+    def open(self):
+        self.conn = sqlite3.connect(self.dbname)
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+
+
+    def get_all_products(self):
+        self.open()
+        self.cursor.execute("""SELECT * FROM products""")
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+    
+    def get_products(self, products_id):
+        self.open()
+        self.cursor.execute("""SELECT * FROM products WHERE id=?""", [products_id])
+        data = self.cursor.fetchone()
+        self.close()
+        return data
+    
+    def get_all_categories(self):
+        self.open()
+        self.cursor.execute("""SELECT * FROM categories""")
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+    
+    def get_categories_products(self, category_id):
+        self.open()
+        self.cursor.execute("""SELECT * FROM products WHERE category_id?""", [category_id])
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+    
+    def add_products(self, title, content, image, user_id, category_id):
+        self.open()
+        self.cursor.execute("""INSERT INTO products(title, content, image, user_id, category_id)
+                            VALUES(?,?,?,?,?)""", [title, content, image, user_id, int(category_id)])       
+        data = self.cursor.fetchall()
+        self.close()
+        return
+    
+    def search_products(self, query):
+        self.open()
+        query = '%' + query + '%'
+        self.cursor.execute("""SELECT * FROM products WHERE (title LIKE ? OR content LIKE ?)""", [query, query])
+        data = self.cursor.fetchall()
+        self.close()
